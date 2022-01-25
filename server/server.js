3139e67cded8aa397263b2e632a2d4746f7f206a
geisonmcd <geisonmcd@gmail.com>
2022-01-25 12:06:10 -0300

    Comment

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors')
 
 app.use(cors({
+    // you have to add the origin that will access, but if anyone can access
+    // it's impossible to add every origin. So I guess this is not for web, but
+    // for purposes when you want to restrict who can access this server?
     origin: 'http://127.0.0.1:5500',
 }))
 
